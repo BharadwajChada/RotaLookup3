3dcb2fe1510468448b9a4fa8134c43813924add1
Bharadwaj Chada <bharadwajchada@Bharadwajs-MacBook-Air.local>
2024-09-27 15:00:15 +0100

    Changed date format

--- app2.py
+++ app2.py
@@ -176,7 +176,8 @@ if sheet_url:
     month = st.selectbox("Select Month", worksheet_names)
 
 day = st.selectbox("Select Day", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-date = st.text_input("Date (e.g., 27th)")
+date = st.selectbox("Select Date", [f"{i}{'th' if 4 <= i <= 20 else 'st' if i % 10 == 1 else 'nd' if i % 10 == 2 else 'rd'}" for i in range(1, 32)])
+
 
 if st.button("Get Rota"):
     sheet = connect_to_google_sheet(sheet_url)
